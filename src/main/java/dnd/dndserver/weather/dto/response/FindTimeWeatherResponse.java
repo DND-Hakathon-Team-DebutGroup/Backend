8097015d9913ccedca58ced018eb487e0b0d1d02
Kim_ju_chan <jckim229@gmail.com>
2024-05-18 06:29:30 +0900

    feat: 수정

--- src/main/java/dnd/dndserver/weather/dto/response/FindTimeWeatherResponse.java
+++ src/main/java/dnd/dndserver/weather/dto/response/FindTimeWeatherResponse.java
@@ -1,6 +1,9 @@
 package dnd.dndserver.weather.dto.response;
 
 import dnd.dndserver.weather.dto.TimeWeatherDTO;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record FindTimeWeatherResponse(
         String nowTemp,
