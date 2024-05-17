706e7cbbaa3f87a7d2fc263e9f07d6a9f91082a1
Kim_ju_chan <jckim229@gmail.com>
2024-05-17 23:06:41 +0900

    feat: UserRepository, RequestDto 추가

--- src/main/java/dnd/dndserver/user/User.java
+++ src/main/java/dnd/dndserver/user/User.java
@@ -1,17 +1,15 @@
 package dnd.dndserver.user;
 
-import jakarta.persistence.CascadeType;
+import dnd.dndserver.file.ImageFile;
+import dnd.dndserver.global.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +18,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 생성자를 통해서 값 변경 목적으로 접근하는 메시지 차단
 @Table(name = "users")
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
