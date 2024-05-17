fde6ce858647608f2dcd0605f774994e92195f27
fakerdeft <whakswp123@naver.com>
2024-05-18 06:20:22 +0900

    feat: 업데이트 시간 추가

--- src/main/java/dnd/dndserver/weather/dto/response/FindTimeWeatherResponse.java
+++ src/main/java/dnd/dndserver/weather/dto/response/FindTimeWeatherResponse.java
@@ -2,11 +2,19 @@ package dnd.dndserver.weather.dto.response;
 
 import dnd.dndserver.weather.dto.TimeWeatherDTO;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public record FindTimeWeatherResponse(
         String nowTemp,
-        String weather
+        String weather,
+        String dateTime
 ) {
     public static FindTimeWeatherResponse from(TimeWeatherDTO timeWeatherDTO) {
-        return new FindTimeWeatherResponse(timeWeatherDTO.getNowTemp(), timeWeatherDTO.getWeather());
+        return new FindTimeWeatherResponse(
+                timeWeatherDTO.getNowTemp(),
+                timeWeatherDTO.getWeather(),
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
+        );
     }
 }
