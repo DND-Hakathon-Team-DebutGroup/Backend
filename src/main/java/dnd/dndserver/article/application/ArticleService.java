be63fc7f64aa1a4028850e19ec7a691211f1fd38
fakerdeft <whakswp123@naver.com>
2024-05-17 23:57:41 +0900

    fix: 게시물 전체 조회 요청 시 동네도 추가

--- src/main/java/dnd/dndserver/article/application/ArticleService.java
+++ src/main/java/dnd/dndserver/article/application/ArticleService.java
@@ -2,10 +2,12 @@ package dnd.dndserver.article.application;
 
 import dnd.dndserver.article.domain.Article;
 import dnd.dndserver.article.dto.request.FindArticleRequest;
+import dnd.dndserver.article.dto.request.SaveArticleRequest;
 import dnd.dndserver.article.dto.response.FindAllArticleResponse;
-import dnd.dndserver.article.repository.ArticleRepository;
+import dnd.dndserver.article.infrastructure.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,10 +17,17 @@ public class ArticleService {
 
     private final ArticleRepository articleRepository;
 
+    @Transactional(readOnly = true)
     public FindAllArticleResponse find(FindArticleRequest request) {
 
-        List<Article> article = articleRepository.findByCityAndDistrict(request.city(), request.district());
+        List<Article> article = articleRepository.findByCityAndDistrict(request.city(), request.district(), request.town());
 
         return FindAllArticleResponse.from(article);
+    }
+
+    @Transactional
+    public void save(SaveArticleRequest request) {
+
+        articleRepository.save(new Article(request));
     }
 }
